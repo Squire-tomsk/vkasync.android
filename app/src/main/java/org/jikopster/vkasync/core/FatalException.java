92b4db55ad7f60bf14a8d675a7269b01ead74216
Игорь Щербаков <i.v.shcherbakov@gmail.com>
2016-02-21 06:47:49 +0600

    added license

--- app/src/main/java/org/jikopster/vkasync/core/FatalException.java
+++ app/src/main/java/org/jikopster/vkasync/core/FatalException.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2016 Jikopster Orglobster.
+ *
+ * This file is part of Jikopster vk a sync
+ *
+ * Jikopster vk a sync is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jikopster vk a sync is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jikopster vk a sync.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.jikopster.vkasync.core;
 
 public interface FatalException { }
